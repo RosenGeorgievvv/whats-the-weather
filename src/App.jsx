b5ef6a73946c5f72d84e20d8cec4620bac578681
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-09-02 17:04:11 +0300

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Weather from './components/Weather'
 
 const App = () => {
   return (
-    <div>Hi</div>
+    <div className='main'>
+      <Weather />
+    </div>
   )
 }
 
