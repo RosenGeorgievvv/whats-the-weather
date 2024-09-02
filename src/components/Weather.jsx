7f5412fd2b749fb7e9588da22586408f5edcf9bb
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-09-02 17:22:03 +0300

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,6 +3,15 @@ import "./Weather.css";
 import search_icon from '../assets/search.png';
 
 const Weather = () => {
+
+    const search = async () =>{
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${import.meta.env.VITE_APP_ID}`
+        } catch (error) {
+            
+        }
+    }
+
   return (
     <div className='weather'>
         <div className='search-bar'>
