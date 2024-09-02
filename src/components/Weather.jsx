c80607c8370f3b76535e6518cdf2c1e72d57be04
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-09-02 17:35:07 +0300

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Weather.css";
 import search_icon from '../assets/search.png';
+import clear_icon from '../assets/clear.png';
+import cloud_icon from '../assets/cloud.png';
+import drizzle_icon from '../assets/drizzle.png';
+import rain_icon from '../assets/rain.png';
+import snow_icon from '../assets/snow.png';
+import wind_icon from '../assets/wind.png';
+import humidity_icon from '../assets/humidty.png';
 
 const Weather = () => {
 
+    const [weather, setWeather] = useState(false);
+
+    const allIcons = {
+        "01d": clea
+    }
+
     const search = async (city) =>{
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
             const response = await fetch(url);
             const data = await response.json();
-            console.log(data);
+            setWeather({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: 
+            })
         } catch (error) {
             
         }
@@ -24,6 +43,25 @@ const Weather = () => {
         <div className='search-bar'>
             <input type='text' placeholder='Search' />
             <img src={search_icon} alt='' />
+        </div>
+        <img src={clear_icon} alt='' className='weather-icon' />
+        <p className='temperature'>16 C</p>
+        <p className='location'>Sofia</p>
+        <div className='weather-data'>
+            <div className='col'>
+                <img src={humidity_icon} alt='' />
+                <div>
+                    <p>91 %</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+            <div className='col'>
+                <img src={wind_icon} alt='' />
+                <div>
+                    <p>3.6 Km/h</p>
+                    <span>Wind Speed</span>
+                </div>
+            </div>
         </div>
     </div>
   )
